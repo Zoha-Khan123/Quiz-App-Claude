e41ed40e35dc3f7e97db66ed2dae317a950e754e
Zoha-Khan123 <zohakhan11004@gmail.com>
2026-04-02 20:13:21 +0500

    backend url add in frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://quiz-app-claude.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
